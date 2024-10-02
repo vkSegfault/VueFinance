532c52a07518df66be5e247a6315abf7949d5606
vkSegfault <adtofaust@gmail.com>
2024-10-02 19:19:05 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template>
+    <h1>Vue Finance</h1>
+    <h2>{{ title }}</h2>
+
+    <p v-if="userActive">User is active</p>
+    <p v-else>User is blocked</p>
+    
+    <p v-if="userStatus === 'active'">User status: {{ userStatus }}</p>
+    <p v-else-if="userStatus === 'pending'">User status: {{ userStatus }}</p>
+
+    <h3>Finance assets:</h3>
+    <ul>
+        <li v-for="asset in finAssets" :key="asset">{{ asset }}</li>
+    </ul>
+
+    <a v-bind:href="link">Click for vkSegfault page</a>
+    <br>
+    <a :href="link">Click for Google</a>
+    <br>
+
+    <button v-on:click="toggleStatus">Change Status</button>
+
+    <form @submit.prevent="addFinAsset">
+        <label for="newFinAsset">Add Finance Asset: </label>
+        <input type="text" id="newFinAsset" name="newFinAsset" v-model="newFinAsset" />
+        <button type="submit">Submit</button>
+    </form>
+</template>
+
+// Composition API - implicit exporting
+<script setup>
+import { ref } from 'vue';
+
+// using ref() allows reactive variables (we can modify them in methods)
+const title = 'Vue Finance Main Page';
+const userActive = ref(true);
+const userStatus = ref('pending');  // active|pending|blocked
+const finAssets = ref(['Stock', 'Bond', 'Invest Fund']);
+const link = 'https://vksegfault.github.io';
+const newFinAsset = ref('...add');
+
+const toggleStatus = () => {
+    if (userStatus.value === 'pending') {
+        userStatus.value = 'active';
+    } else if(userStatus.value === 'active') {
+        userStatus.value = 'pending';
+    } else {
+
+    }
+};
+
+const addFinAsset = () => {
+    if (newFinAsset.value.trim() !== '') {
+        finAssets.value.push(newFinAsset.value);
+        newFinAsset.value = '';
+    }
+};
+
+</script>
+
+// Options API
+<!-- <script>
+// we must export any data that we want to use in <template>
+export default {
+    data() {
+        return {
+            title: 'Vue Finance Main Page',
+            userActive: true,  // true|false
+            userStatus: 'pending',  // active|pending|blocked
+            finAssets: ['Stock', 'Bond', 'Invest Fund'],
+            link: 'https://vksegfault.github.io'
+        }
+    },
+
+    methods: {
+        toggleStatus() {
+            if (this.userStatus === 'pending') {
+                this.userStatus = 'active'
+            } else if(this.userStatus === 'active') {
+                this.userStatus = 'pending'
+            } else {
+
+            }
+        }
+    }
+}
+</script> -->
